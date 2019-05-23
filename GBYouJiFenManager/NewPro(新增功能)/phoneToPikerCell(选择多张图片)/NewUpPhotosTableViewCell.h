8779133cb308b98c413230f5aa210476a432d141
秦根 <290142650@qq.com>
2019-05-23 17:47:05 +0800

    二期修改+

---
+++ GBYouJiFenManager/NewPro(新增功能)/phoneToPikerCell(选择多张图片)/NewUpPhotosTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  NewUpPhotosTableViewCell.h
+//  GBYouJiFenManager
+//
+//  Created by chenheng on 2019/5/23.
+//  Copyright © 2019年 张卫煌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewUpPhotosTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
