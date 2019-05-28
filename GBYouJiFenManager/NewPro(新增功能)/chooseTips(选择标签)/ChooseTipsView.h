ae878cd71e5c4153a0716181607b730c836a045a
秦根 <290142650@qq.com>
2019-05-28 18:51:18 +0800

    ChooseTipsView

--- GBYouJiFenManager/NewPro(新增功能)/chooseTips(选择标签)/ChooseTipsView.h
+++ GBYouJiFenManager/NewPro(新增功能)/chooseTips(选择标签)/ChooseTipsView.h
@@ -9,9 +9,13 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
-
+@class TipsModel;
 @interface ChooseTipsView : UIView
 +(void)startChooseTipsCallBack:(void(^)(NSString * values))callback;
 @end
 
+@interface TipsModel : NSObject
+@property(nonatomic,copy)NSString * name;
+@property(nonatomic,assign)BOOL isSelected;
+@end
 NS_ASSUME_NONNULL_END
--- GBYouJiFenManager/NewPro(新增功能)/chooseTips(选择标签)/ChooseTipsView.m
+++ GBYouJiFenManager/NewPro(新增功能)/chooseTips(选择标签)/ChooseTipsView.m
@@ -7,25 +7,73 @@
 //
 
 #import "ChooseTipsView.h"
-@interface ChooseTipsView()
+@interface ChooseTipsView()<UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
 @property(nonatomic,copy)void(^chooseDone)(NSString * values);
+@property(nonatomic,strong)UIView *contentView;
+@property(nonatomic,strong)UICollectionView * collectionView;
+@property(nonatomic,strong)NSArray * dataArray;
 @end
 @implementation ChooseTipsView
 +(void)startChooseTipsCallBack:(void(^)(NSString * values))callback
 {
     ChooseTipsView * temp  = [[ChooseTipsView alloc]init];
-    temp.backgroundColor = [UIColor whiteColor];
+    temp.frame = UIApplication.sharedApplication.keyWindow.bounds;
     [UIApplication.sharedApplication.keyWindow addSubview:temp];
-    [temp mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.centerY.mas_equalTo(UIApplication.sharedApplication.keyWindow);
-        make.size.mas_equalTo(CGSizeMake(SCALE(345), SCALE(345)));
-    }];
+
     
 }
 
 -(instancetype)init
 {
     if (self = [super init]) {
+         self.backgroundColor = A_COLOR_STRING(0x191919, 0.8f);
+        UIView *contenView = [[UIView alloc]initWithFrame:CGRectMake(0, SCREEN_HEIGHT, SCREEN_WIDTH, 515)];
+        self.contentView = contenView;
+        self.contentView.backgroundColor = [UIColor whiteColor];
+        [self addSubview:self.contentView];
+        
+        UILabel * titleLabel = [[UILabel alloc]init];
+        UICollectionViewFlowLayout * layout = [[UICollectionViewFlowLayout alloc]init];
+        UICollectionView * collectionview = [[UICollectionView alloc]initWithFrame:CGRectZero collectionViewLayout:layout];
+        [collectionview registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"UICollectionViewCell"];
+        collectionview.delegate = self;
+        collectionview.dataSource = self;
+        self.collectionView = collectionview;
+        UITextField * textfield = [[UITextField alloc]init];
+        UIButton * btn1 = [UIButton buttonWithType:UIButtonTypeCustom];
+        UIButton * btn2 = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn1.backgroundColor = MainColor;
+        [self.contentView addSubview:titleLabel];
+        [self.contentView addSubview:collectionview];
+        [self.contentView addSubview:textfield];
+        [self.contentView addSubview:btn1];
+        [self.contentView addSubview:btn2];
+        [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.left.right.mas_equalTo(self.contentView);
+            make.height.mas_equalTo(50);
+        }];
+        [collectionview mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.mas_equalTo(titleLabel.bottom);
+            make.left.right.mas_equalTo(self.contentView);
+            make.height.mas_equalTo(200);
+        }];
+        [btn1 mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.mas_equalTo(collectionview.bottom);
+            make.right.mas_equalTo(self.contentView).offset(-15);
+            make.size.mas_equalTo(CGSizeMake(80, 45));
+        }];
+        [textfield mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.mas_equalTo(collectionview.bottom);
+            make.left.mas_equalTo(self.contentView).offset(2);
+            make.right.mas_equalTo(btn1.mas_left).offset(-10);
+            make.height.mas_equalTo(45);
+        }];
+       
+        [btn2 mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.mas_equalTo(textfield.mas_bottom).offset(20);
+            make.left.right.mas_equalTo(self.contentView).offset(50);
+            make.height.mas_equalTo(50);
+        }];
         [self getdata];
     }
     return self;
@@ -33,21 +81,35 @@
 -(void)getdata{
     [SVProgressHUD showWithStatus:@"加载中"];
     NSDictionary *dic=@{@"FromTableName":@"CMS_BaseVar",@"SelectField":@"*",@"Condition":@"moduleno$=$CMS$AND$varfield$=$shoplabel",@"SelectOrderBy":@"",@"CipherText":CIPHERTEXT};
+    MJWeakSelf;
     [[NetDataTool shareInstance]getNetData:ROOTPATH url:@"/SystemCommService.asmx/GetCommSelectDataInfo3" With:dic and:^(id responseObject) {
         
         NSDictionary *dic1=[JsonTools getData:responseObject];
         NSLog(@"dic1dic1==>%@",dic1);
-        
+
+//        m = dic1@[@""];
+        [weakSelf.collectionView reloadData];
     } Faile:^(NSError *error) {
         NSLog(@"失败%@",error);
     }];
 }
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+
+#pragma mark - collectionView
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return self.dataArray.count;
 }
-*/
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UICollectionViewCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"UICollectionViewCell" forIndexPath:indexPath];
+    return cell;
+}
+
+@end
+
+
+@implementation TipsModel
+
+
 
 @end
